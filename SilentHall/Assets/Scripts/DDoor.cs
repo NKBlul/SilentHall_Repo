2165ff52d6819216726832b45889a12ba81b164e
Hansen Harly <hansenharly123@gmail.com>
2024-11-20 02:40:36 -0500

    base door added and front doors

---
+++ SilentHall/Assets/Scripts/DDoor.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DDoor : BaseDoor, IInteractable
+{
+    public string GetInteractionPrompt()
+    {
+        if (!isOpen)
+        {
+            return $"Press [E] to open door";
+        }
+
+        return $"Press [E] to close door";
+    }
+
+    public void OnInteract()
+    {
+        if (!isOpen)
+        {
+            animator.Play(DoorAnimationState.Door_Open.ToString());
+            isOpen = true;
+        }
+        else
+        {
+            animator.Play(DoorAnimationState.Door_Close.ToString());
+            isOpen = false;
+        }
+    }
+}
