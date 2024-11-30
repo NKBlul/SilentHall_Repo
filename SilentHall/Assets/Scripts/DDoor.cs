66e4a18b85b76b74313bb392077f9ac67adb7e76
Hansen Harly <hansenharly123@gmail.com>
2024-11-29 23:48:09 -0500

    double door added for 2nd stair, and script

---
+++ SilentHall/Assets/Scripts/DDoor.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class DDoor : MonoBehaviour, IInteractable
+{
+    [SerializeField] GameObject leftDoor;
+    [SerializeField] GameObject rightDoor;
+
+    Vector3 openLeftDoorRot = new Vector3(0, -90, 0);
+    Vector3 openRightDoorRot = new Vector3(0, 90, 0);
+
+    float openSpeed = 2f;
+    //bool isOpen = false;
+    bool isLocked = true;
+
+    public string GetInteractionPrompt()
+    {
+        if (isLocked)
+        {
+            return $"Door is Locked, Find a Key";
+        }
+        else
+        {
+            //if (!isOpen)
+            //{
+            //    return $"Press [E] to open door";
+            //}
+            //return $"Press [E] to close door";
+            return $"Press [E] to open door";
+        }
+    }
+
+
+    public void OnInteract()
+    {
+        if (isLocked)
+        {
+            isLocked = false;
+        }
+        else
+        {
+            //if (!isOpen)
+            //{
+            //
+            //}
+            //else
+            //{
+            //
+            //}
+            //isOpen = !isOpen;
+            Debug.Log($"Opening door");
+            StartCoroutine(OpenDoor(leftDoor, openLeftDoorRot));
+            StartCoroutine(OpenDoor(rightDoor, openRightDoorRot));
+        } 
+    }
+
+    IEnumerator OpenDoor(GameObject door, Vector3 endRot)
+    {
+        float elapsedTime = 0f;
+        Quaternion startRotation = door.transform.localRotation;
+        Quaternion endRotation = Quaternion.Euler(endRot);
+
+        while (elapsedTime < openSpeed)
+        {
+            door.transform.localRotation = Quaternion.Lerp(startRotation, endRotation, elapsedTime / openSpeed);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        door.transform.localRotation = endRotation; // Ensure final rotation is correct.
+    }
+}
