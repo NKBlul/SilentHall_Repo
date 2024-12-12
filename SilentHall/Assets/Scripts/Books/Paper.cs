169316c36b2f9fa83c0a053b55eceb4ccce41db7
Hansen Harly <hansenharly123@gmail.com>
2024-12-11 21:59:07 -0500

    added shelf  and cart models and texture
    biology lab updated to have text on board

--- SilentHall/Assets/Scripts/Books/Paper.cs
+++ SilentHall/Assets/Scripts/Books/Paper.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Paper : MonoBehaviour, IInteractable
 {
     public PaperData paperData;
+    public bool isReading;
 
     public string GetInteractionPrompt()
     {
-        return $"Press [E] to read the paper";
+        if (!isReading)
+        {
+            return $"Press [E] to read the paper";
+        }
+        return "";
     }
 
     public void OnInteract()
     {
-        
+        if (!isReading)
+        {
+            //UIManager.instance.paper.SetActive(true);
+            //UIManager.instance.ChangeText(UIManager.instance.paperText, paperData.paperText);
+        }
     }
 }
--- SilentHall/Assets/Scripts/Books/PaperData.cs
+++ SilentHall/Assets/Scripts/Books/PaperData.cs
@@ -7,21 +7,3 @@ public class PaperData : ScriptableObject
     [TextArea(15, 20)]
     public string paperText;
 }
-
-//[CustomEditor(typeof(PaperData))]
-//public class PaperDataEditor : Editor
-//{
-//    SerializedProperty textBox;
-//
-//    private void OnEnable()
-//    {
-//        textBox = serializedObject.FindProperty("paperText");
-//    }
-//
-//    public override void OnInspectorGUI()
-//    {
-//        base.OnInspectorGUI();
-//
-//
-//    }
-//}
