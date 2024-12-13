76e326d0120d69aed0d2ae96e326b931763d7464
Hansen Harly <hansenharly123@gmail.com>
2024-12-13 17:03:06 -0500

    lock can now rotate properly

--- SilentHall/Assets/Scripts/Gear.cs
+++ SilentHall/Assets/Scripts/Gear.cs
@@ -1,10 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class Gear : MonoBehaviour
 {
+    int rotateAmount = 36;
+    int gearIndex = 0;
+
+    private void OnEnable()
+    {
+        transform.localRotation = Quaternion.identity;
+    }
+
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) // Left mouse button click
@@ -20,8 +29,21 @@ public class Gear : MonoBehaviour
                 if (hit.collider.gameObject == gameObject)
                 {
                     Debug.Log($"{gameObject.name} was clicked!");
+                    RotateGear();
                 }
             }
+        }
+    }
+
+    void RotateGear()
+    {
+        transform.Rotate(Vector3.up, 36f, Space.Self);  // Rotate 36 degrees around the local Y-axis
+
+        gearIndex++;
+
+        if (gearIndex + 1 > 9)
+        {
+            gearIndex = 0;
         }
     }
 }
